a8d6a746e163997fb8e41385895fd9212b738f6f
ioblackshaw <isaacoblackshaw@gmail.com>
2025-12-28 20:04:39 +0000

    initial

---
+++ src/main/java/games/enchanted/eg_template_mod/common/ModConstants.java
@@ -0,0 +1,14 @@
+package games.enchanted.eg_template_mod.common;
+
+public class ModConstants {
+    public static final String MOD_NAME = "Enchanted's Template Mod";
+    public static final String MOD_ID = "eg_template_mod";
+
+    public static final String TARGET_PLATFORM =
+    //? if fabric {
+        "fabric"
+    //?} else {
+        /*"neoforge"
+     *///?}
+    ;
+}
